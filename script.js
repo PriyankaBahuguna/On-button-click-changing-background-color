f49347e8e843fdcf82d392c5071e2a506c35be7a
Priyanka Bahuguna <bahugunapriyanka1@gmail.com>
2023-01-12 17:48:13 +0530

    on button click changing background color
    on button click changing background color

---
+++ script.js
@@ -0,0 +1,7 @@
+// const color=["red","blue","green","yellow","pink"]
+
+// function colorChange(){
+//     const colorIndex= parseInt(Math.random()*color.length)
+//     document.body.style.background= color[colorIndex]
+// }
+document.write(math.random())
